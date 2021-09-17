59429a97f40c6cb09575a0e75d8ab200991770c3
abuchanan120 <61256045+abuchanan120@users.noreply.github.com>
2021-09-17 14:46:56 -0400

    Programming Assignment 2 Changes + comments

--- main.py
+++ main.py
@@ -3,14 +3,15 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 def print_inputs():
+    #Ask for name and print to output
     print("What is your name?")
     name=input()
     print("Your name is:",name)
-
+    #Ask for a number, square it, and display it
     print("Enter a number:")
     number=int(input())
     print(number, " multiplied by ",number,"= ",number * number,sep="")
-
+    #Ask for a word, display the number of letters
     print("Enter a word:")
     word=input()
     print("Number of letters in ",word,": ",word.__len__(),sep="")
@@ -23,5 +24,5 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_inputs()
+    print_inputs() #run the print_inputs() function
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
