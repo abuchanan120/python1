3957c48ed9703e6635789d005e32f53f368e848b
abuchanan120 <61256045+abuchanan120@users.noreply.github.com>
2021-09-17 14:40:33 -0400

    Programming Assignment 2 Changes

--- main.py
+++ main.py
@@ -2,6 +2,18 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def print_inputs():
+    print("What is your name?")
+    name=input()
+    print("Your name is:",name)
+
+    print("Enter a number:")
+    number=int(input())
+    print(number, "multiplied by itself=",number * number)
+
+    print("Enter a word:")
+    word=input()
+    print("Number of letters in ",word,": ",word.__len__(),sep="")
 
 
 def print_hi(name):
@@ -11,6 +23,5 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Andrew')
-
+    print_inputs()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
