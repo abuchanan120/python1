99889d09fdc3df0c7304c5a71ab0242549933565
abuchanan120 <61256045+abuchanan120@users.noreply.github.com>
2021-10-23 13:10:39 -0400

    added random branch feature

--- main.py
+++ main.py
@@ -5,55 +5,13 @@
 
 import random
 
-
-def print_inputs():
-    #Ask for name and print to output
-    print("What is your name?")
-    name=input()
-    print("Your name is:",name)
-    #Ask for a number, square it, and display it
-    print("Enter a number:")
-    number=int(input())
-    print(number, " multiplied by ",number,"= ",number * number,sep="")
-    #Ask for a word, display the number of letters
-    print("Enter a word:")
-    word=input()
-    print("Number of letters in ",word,": ",word.__len__(),sep="")
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-def array_art():
-    arr=[['.', 'O', '.', 'O', '.', 'O', '.'],
-        ['.', 'O', '.', 'O', '.', 'O', '.'],
-        ['O', '.', 'O', '.', 'O', '.', 'O'],
-        ['O', '.', 'O', '.', 'O', '.', 'O'],
-        ['.', 'O', '.', 'O', '.', 'O', '.'],
-        ['O', '.', 'O', '.', 'O', '.', 'O'],
-        ['O', '.', 'O', '.', 'O', '.', 'O'],
-        ['.', 'O', '.', 'O', '.', 'O', '.'],
-        ['.', 'O', '.', 'O', '.', 'O', '.']]
-    for i in range(len(arr[0])): #for every column (length of each row = number of columns)
-        for k in range(len(arr)): #select a row (length of the array = number of rows)
-            print(arr[k][i], end="") #print the element in row,col
-        print() #carriage-return
-
-def coin_flip():
-    #Prompt for user choice
-    print("Please choose either 0 or 1:")
-    choice=int(input())
-
-    #Choose 0 or 1 randomly
-    answer=random.randint(0, 1)
-
-    #Figure out if choice is correct
-    if choice==answer:
-        print("You win!")
-    else:
-        print("The Computer wins!")
+def random_avg():
+    sum = 0
+    for i in range(10):
+        sum += random.randint(0,1000)
+    print("The average of 10 random integers from 0-1000:", sum/10)
 
 # Main code execution
 if __name__ == '__main__':
-
+    random_avg()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
