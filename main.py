d7627c3b94994dfbee9b8b8f940e180e882270cb
abuchanan120 <61256045+abuchanan120@users.noreply.github.com>
2021-10-28 12:34:01 -0400

    added strings1 branch

--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import random
-import platform
+import sys
 
 
-def os_desc():
-    print("Your system is running:", platform.platform(), "with a(n)", platform.machine(), "processor.")
 
 # Main code execution
 if __name__ == '__main__':
-    os_desc()
+    print(f"Arguments Count: {len(sys.argv)}")
+    for i, arg in enumerate(sys.argv):
+        print(f"Argument {i:>6}: {arg}")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
