3a90caef7fe315d446046b2e0119cd7a14102bc5
abuchanan120 <61256045+abuchanan120@users.noreply.github.com>
2021-10-28 13:28:14 -0400

    completed strings3 branch

--- main.py
+++ main.py
@@ -4,28 +4,33 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-#Converts a given string to pig latin
-def pigLatin(str):
-    str = str.lower()
-    words = str.split()
-    for i, word in enumerate(words):
-        if word[0] in 'aeiou':                  #if the first letter is a vowel
-            words[i] = words[i] + "yay"         #add yay to the end
-        else:
-            has_vowel = False
-            for j, letter in enumerate(word):
-                if letter in 'aeiou':            #if first letter is a consonant, look for next vowel
-                    words[i] = word[j:] + word[:j] + "ay"       #then move the consonant(s) to the end and add ay
-                    has_vowel = True
-                    break
+#Breaks up a given string with split()
+def breakUp1(str):
+    new = str.split()
+    for i in new:
+        print(i)
 
-            if(has_vowel == False):             #if there are no vowels, just append "ay"
-                words[i] = words[i] + "ay"
-    newstr = ' '.join(words)
-    print("Pig Latin:", newstr)
+#Breaks up a given string without split()
+def breakUp2(str):
+    list = []
+    temp = ""
+    #Break up the string into a list
+    for i in str:
+        if i == " ":
+            list.append(temp)
+            temp = ""
+        else:
+            temp += i
+    if temp:
+        list.append(temp)
+
+    #Print the string
+    for k in list:
+        print(k)
 
 # Main code execution
 if __name__ == '__main__':
     string=input("Enter a string:")
-    pigLatin(string)
+    breakUp1(string)
+    breakUp2(string)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
