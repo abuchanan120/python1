cd9e6eeb6a628361e6f0ff4a4d3e7ffba81c1701
abuchanan120 <61256045+abuchanan120@users.noreply.github.com>
2021-11-04 13:55:37 -0400

    added comments and final touches

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 # Andrew Buchanan Python 1
 # Programming Assignment 7
 # November 4, 2021
-# Using re and os modules to experiment with regex and os output
+# Using re and os (and platform) modules to experiment with regex and os output
 
 import re, os, platform   #the os.uname() appears to only work on UNIX systems,
-                #but platform.uname() can be used on Windows
+                #but platform.uname() can be used on Windows, so I imported that to use instead.
 
 # Main code execution
 if __name__ == '__main__':
     str1 = str(platform.uname())    #os.uname() doesn't work for Windows
-    string = str(os.getcwd())
+    string = str(os.getcwd())       #os.getcwd() prints the current working directory
+
     #Regex searching
     result = re.search("Windows", str1)     #Matches because string contains Windows
-    print(result)
+    print("1.", result)
     result = re.search("Windows|Linux", str1)   #Matches because at least one of Windows/Linux were found
-    print(result)
-    result = re.search("AMD64", str1)       #Matches because string contains AMD64
-    print(result)
+    print("2.", result)
+    result = re.findall("AMD64|version|system|Intel", str1)       #Finds all patterns and stores in a list
+    print("3.", result)
     result = re.match("uname_result", str1) #Matches because string starts with pattern
-    print(result)
+    print("4.", result)
     result = re.match("version", str1)      #Won't match because string doesn't start with version
-    print(result)
+    print("5.", result)
     result = re.search("Users", string)     #Matches because Users is in the string
-    print(result)
+    print("6.", result)
